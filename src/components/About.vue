cb4d5c111b758d957c9d9ebe4c1e24bf8b8e0848
32202734ar <haghshenas052@gmail.com>
2022-06-07 10:07:07 +0430

    init project

---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1>
+    About
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet blanditiis cumque doloribus ex fugiat minima minus nam nulla quidem similique, sint, ullam veniam? Est illo illum itaque provident quidem?
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+
+</style>
